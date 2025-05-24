5fa9e6f4d5e61b871137400fe1108fddd735c6a8
atan333 <amytan@terpmail.umd.edu>
2025-05-24 13:12:41 -0400

    Initial commit

---
+++ src/main/java/com/amytan/amys_blog/AmysBlogApplication.java
@@ -0,0 +1,13 @@
+package com.amytan.amys_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmysBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmysBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amytan/amys_blog/AmysBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amytan.amys_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmysBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
